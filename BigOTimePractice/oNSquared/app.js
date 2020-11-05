9aa43c4ebeacc788f8ffdac3a28b90b0a9affcd7
ed1868 <ruizeduardo21@gmail.com>
2020-11-05 11:33:43 -0500

    oNSquared functions)

---
+++ BigOTimePractice/oNSquared/app.js
@@ -0,0 +1,10 @@
+studentArrCheck = (arr) => {
+  arr.map((studentInfoArr) => {
+    studentInfoArr.map((studentInfo) => {
+      console.log(studentInfo);
+    });
+  });
+};
+
+
+studentArrCheck();
