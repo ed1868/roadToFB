15c4b9bfcd4fd11c2248f5c09895f9702cd7def5
ed1868 <ruizeduardo21@gmail.com>
2020-11-07 00:49:50 -0500

    numberOfMoves

--- leetCode/numberOfMoves/app.js
+++ leetCode/numberOfMoves/app.js
@@ -2,10 +2,27 @@
  * @param {number[]} heights
  * @return {number}
  */
-var heightChecker = function(heights) {
-    // CHECK FOR CONSTRAINTS -- HEIGHTS LENGTH IS LESS THAN 100 
-    // HEIGHT ITSELF IS LESS THAN 100
-    // SORT THE ARRAY IN INCREASING ORDER WITHOUT MOVING ELEMENTS THAT ARE IN THE RIGHT ORDER
-    // CREATE COUNTER TO KEEP TRACK OF EVERY STUDENT THAT MOVES
-    // FOR EVERY STUDENT THAT MOVES, COUNTER GOES UP 
+heightChecker = (heights) => {
+  // CREATE COUNTER TO KEEP TRACK OF EVERY STUDENT THAT MOVES
+  let counter = 0;
+  // CHECK FOR CONSTRAINTS -- HEIGHTS LENGTH IS LESS THAN 100
+  if (heights.length < 100) {
+    heights.sort(function (a, b) {
+      // HEIGHT ITSELF IS LESS THAN 100
+      if (a > 100 || b > 100) {
+        return;
+      }
+      if(a > b){
+        counter += 1
+      }
+
+      return a-b;
+    });
+
+    console.log(counter)
+  }
+  // SORT THE ARRAY IN INCREASING ORDER WITHOUT MOVING ELEMENTS THAT ARE IN THE RIGHT ORDER
+  // FOR EVERY STUDENT THAT MOVES, COUNTER GOES UP
 };
+
+heightChecker([1, 1, 4, 2, 1, 3]);
