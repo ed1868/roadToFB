e5c04b46fb88e9a67d065d353f9950358b74245d
ed1868 <ruizeduardo21@gmail.com>
2020-11-16 09:24:37 -0500

    solution to palindrome practice

--- leetCode/easyLevel/palindromeNums/palindrome.js
+++ leetCode/easyLevel/palindromeNums/palindrome.js
@@ -1,7 +1,9 @@
-
-let palindrome = (num) => {
-
-}
+let isPalindrome = (num) => {
+  if (num.toString().split("").reverse().join("") == num) {
+    return true;
+  }
+  return false
+};
 
 var testCaseNumber = 1;
 
@@ -30,10 +32,21 @@ function check(expected, output) {
 
 var int = -123;
 var expectedOne = false;
-var outputOne = palindrome(int);
+var outputOne = isPalindrome(int);
 check(expectedOne, outputOne);
 
 var int2 = 121;
 var expectedTwo = true;
-var outputTwo = palindrome(int2);
+var outputTwo = isPalindrome(int2);
 check(expectedTwo, outputTwo);
+
+
+var int3 = 714;
+var expectedThree = false;
+var outputThree = isPalindrome(int3);
+check(expectedThree, outputThree);
+
+var int4 = 10;
+var expectedFour = false;
+var outputFour = isPalindrome(int4);
+check(expectedFour, outputFour);
