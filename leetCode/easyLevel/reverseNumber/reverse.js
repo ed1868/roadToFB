1470409ef17c2d2560899c2df85633d128333ea5
ed1868 <ruizeduardo21@gmail.com>
2020-11-15 20:42:07 -0500

    completed reverse leet code function but ask ronald about the last if statement

--- leetCode/easyLevel/reverseNumber/reverse.js
+++ leetCode/easyLevel/reverseNumber/reverse.js
@@ -2,15 +2,19 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    
+var reverse = function (x) {
+  let result = parseFloat(x.toString().split("").reverse().join(""));
+  console.log('siht',Math.pow(-2, 31))
+  if (result < Math.pow(-2, 31) || result > Math.pow(2, 31)) {
+    return 0;
+    }
+  return result * Math.sign(x);
 };
-
 
 // TEST CASES FOR PROBLEM
 
 function printString(str) {
-  console.log(typeof(str))
+  console.log(typeof str);
   var out = '["' + str + '"]';
   return Array(out);
 }
@@ -18,12 +22,12 @@ function printString(str) {
 var testCaseNumber = 1;
 
 function check(expected, output) {
-  console.log('ex:',  expected);
-  console.log('out: ', output);
+  console.log("ex:", expected);
+  console.log("out: ", output);
 
   var result = expected == output;
 
-  console.log('my result: ', result)
+  console.log("my result: ", result);
   var rightTick = "\u2713";
   var wrongTick = "\u2717";
   if (result) {
@@ -40,9 +44,12 @@ function check(expected, output) {
   testCaseNumber++;
 }
 
-
-
-var int = 123;
-var expectedOne = 321;
+var int = -123;
+var expectedOne = -321;
 var outputOne = reverse(int);
 check(expectedOne, outputOne);
+
+var int2 = 198490;
+var expectedTwo = 094819;
+var outputTwo = reverse(int2);
+check(expectedTwo, outputTwo);
