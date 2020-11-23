38e1cd8de3993f7037a04d33ef64275c5519fa68
ed1868 <ruizeduardo21@gmail.com>
2020-11-23 08:50:19 -0500

    balanced tree solution

--- leetCode/easyLevel/balancedBinaryTree/isBalanced.js
+++ leetCode/easyLevel/balancedBinaryTree/isBalanced.js
@@ -1,3 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+isBalanced = (root) => {
+  if (root == null) {
+    return true;
+  }
+
+  return treeHeight(root) != -1;
+};
+
 let testCaseNumber = 1;
 
 check = (expected, output) => {
