59ad5537dbe7b7900caa59168c9ade5f6653ed05
ed1868 <ruizeduardo21@gmail.com>
2020-11-29 10:49:41 -0500

    spinalCase complete

--- freeCode/spinalCase/spinalCase.js
+++ freeCode/spinalCase/spinalCase.js
@@ -1,5 +1,13 @@
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
 spinalCase = (str) => {
-  return str;
+  // Create a variable for the white space and underscores.
+  let regex = /\s+|_+/g;
+  // Replace the camelCases by spaces
+  let newStr = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  // Replace space and underscores with -
+  return newStr.replace(regex, "-").toLowerCase();
 };
 
 spinalCase("This Is Spinal Tap");
+spinalCase("thisIsSpinalTap");
