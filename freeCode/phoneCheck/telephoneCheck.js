01d1902cabd8568731922856a91dbe013d4d3800
ed1868 <ruizeduardo21@gmail.com>
2021-01-12 09:55:00 -0500

    romeo and juliet problem

---
+++ freeCode/phoneCheck/telephoneCheck.js
@@ -0,0 +1,13 @@
+function telephoneCheck(str) {
+  //turn str to array
+  //check if array includes letters
+  //if it does, return false
+  //Check the length of the array
+  // if its 10, procceed
+  // if its 11, check that the first item is a 1
+  //Check if element is a number
+  // if its not check that it is either a space, dash or parenthesis
+  // every parenthesis must have an opening and closing to be true
+}
+
+telephoneCheck("555-555-5555");
